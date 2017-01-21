9f206177a5d5ef6f75eaeb87182de0b98a7cc030
CJdog89 <cjerskine@gmail.com>
2017-01-21 17:10:35 -0500

    Creating py file for Fishing

--- DiscordScape.py
+++ DiscordScape.py
@@ -9,38 +9,52 @@ DiscordAPI = 'MjcyNDU4ODA3NzU2NzE4MDgw.C2VSvQ.kcERTwPL3D7MzBvVhFaLYqJRF3I'
 
 #Trigger for Help Command
 helpTrigger = '>help'
+helpF = "helpFile.txt"
 # Trigger for Fishing
 fishTrigger = '>fish'
+fishF = "fishFile.txt"
 #Trigger for Selling Items
 sellTrigger = '>sell'
+sellF = "sellFile.txt"
+#Trigger for Inventory
+invTrigger = '>inv'
+invF = "inventory.txt"
 
 
-
-output = ""
 client = discord.Client()
 
 # Bot Ready messages
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
 
 # Checks Chat for trigger phrase
 @client.event
 async def on_message(message):
-    # If author is the bot itself
-    if message.author == client.user:
-        return
-    # Fishing
-    if message.content.startswith(fishTrigger):
-        await client.send_message(message.channel, "You attempt to fish...")
-        return
-    # Help Message
-    if message.content.startswith(helpTrigger):
-        await client.send_message(message.channel, "```How can I help? \n1) Fishing\n2) Mining (i guess)\n3) Selling```")
-        return
+	# If author is the bot itself
+	if message.author == client.user:
+		return
+	# Fishing
+	if message.content.startswith(fishTrigger):
+		await client.send_message(message.channel, "You attempt to fish...")
+		await asyncio.sleep(5)
+		await client.send_message(message.channel, "You caught... a :fish:!")
+		return
+	# Help Message
+	if message.content.startswith(helpTrigger):
+		f = open(helpF)
+		await client.send_message(message.channel, f.read())
+		f.close()
+		return
+	#Test Inventory
+	if message.content.startswith(invTrigger):
+		f = open(invF)
+		await client.send_message(message.channel, f.read())
+		f.close()
+		return
  
 # Discord API key
 client.run(DiscordAPI)
