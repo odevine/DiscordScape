a6cb5b77c1b11f5a9b1e9ab0bbb2299e2e7e9ab5
ode <0d3vine@gmail.com>
2017-01-26 17:50:16 -0500

    test commit

--- fish.py
+++ fish.py
@@ -1,9 +1,12 @@
 class fish:
+
 	def cast(player, location):
 		#temp code to show that the player name and location are stored
 		output = "Player: %s Location: %s" % (player, location)
 		return output
+
 	def time(player, location):
 		return 3
+
 	def caught(player, location):
 		return 'You caught a fishy'
